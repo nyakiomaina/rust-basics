ae741dce50bd34b848acf17bc5ab5c387abe88a4
nyakiomaina <carolinenyakio21@email.com>
2022-12-30 16:12:41 +0300

    variables and data interacting with clone

---
+++ ownership/clone/target/debug/clone.d
@@ -0,0 +1,1 @@
+/home/nyakio/rust-basics/ownership/clone/target/debug/clone: /home/nyakio/rust-basics/ownership/clone/src/main.rs
---
+++ ownership/clone/target/debug/deps/clone-00117942b0ce82c1.d
@@ -0,0 +1,5 @@
+/home/nyakio/rust-basics/ownership/clone/target/debug/deps/clone-00117942b0ce82c1: src/main.rs
+
+/home/nyakio/rust-basics/ownership/clone/target/debug/deps/clone-00117942b0ce82c1.d: src/main.rs
+
+src/main.rs:
